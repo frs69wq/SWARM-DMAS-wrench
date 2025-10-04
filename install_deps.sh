5e384b7c0702c647df67f2023ebad090dcb72770
Fred Suter <suterf@ornl.gov>
2025-10-04 16:31:34 -0400

    fix install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,6 @@ sudo apt install -y cmake libboost-all-dev jed nlohmann-json3-dev
 
 wget --no-check-certificate https://framagit.org/simgrid/simgrid/-/archive/v4.0/simgrid-v4.0.tar.gz && tar -xf simgrid-v4.0.tar.gz && cd simgrid-v4.0 && cmake . && make -j`nproc` && sudo make install && cd .. && rm -rf simgrid-v4.0*
 wget --no-check-certificate https://github.com/simgrid/file-system-module/archive/refs/tags/v0.3.tar.gz && tar -xf v0.3.tar.gz && cd file-system-module-0.3  && cmake . && make -j`nproc` && sudo make install && cd .. && rm -rf file-system-module-0.3*
-git clone --depth=1 https://github.com/wrench-project/wrench.git && cd wrench && cmake . && make -j`nproc` && sudo make install && cd .. && rm -rf DTLMod*
+git clone --depth=1 https://github.com/wrench-project/wrench.git && cd wrench && cmake . && make -j`nproc` && sudo make install && cd .. && rm -rf wrench*
 
 rm -rf build && mkdir build && cd build && cmake .. && make -j`nproc` && cd ..
