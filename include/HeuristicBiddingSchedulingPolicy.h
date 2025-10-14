e28245b53c9920eb8a1da1cfbfcb27a2e9775c5f
Fred Suter <suterf@ornl.gov>
2025-10-13 23:48:53 -0400

    Add Prachi's heuristic ... to be revised

--- include/HeuristicBiddingSchedulingPolicy.h
+++ include/HeuristicBiddingSchedulingPolicy.h
@@ -6,6 +6,7 @@
 #include "ControlMessages.h"
 #include "JobSchedulingAgent.h"
 #include "SchedulingPolicy.h"
+#include "utils.h"
 
 class HeuristicBiddingSchedulingPolicy : public SchedulingPolicy {
 
@@ -25,11 +26,47 @@ public:
                      const std::shared_ptr<HPCSystemStatus>& hpc_system_status) override
   {
     // TODO replace by a heuristic based on job description, system description, and system status
-    std::random_device rd;  // Seed
-    std::mt19937 gen(rd()); // Mersenne Twister engine
-    std::uniform_real_distribution<double> dis(0.0, std::nextafter(1.0, 2.0));
+    // 1. Feasibility check: if the job does not pass the acceptance tests, bid is 0.0
+    // TODO test storage in acceptance tests
+    if (not do_pass_acceptance_tests(job_description, hpc_system_description))
+      return 0.0;
 
-    return dis(gen);
+    // 2. Utilization-based scores
+    auto node_score =
+        1 - (hpc_system_description->get_num_nodes() - hpc_system_status->get_current_num_avaibable_nodes()) /
+                (1.0 * hpc_system_description->get_num_nodes());
+    // FIXME: this is equivalent to the previous score
+    // mem_score  = 1 - (machine.used_memory / machine.total_memory)
+    // TODO see if we can add storage (not a the moment)
+    // stor_score = 1- (machine.used_storage / machine.total_storage)
+
+    // 3. Compatibility
+    auto node_compat =
+        std::min(1.0, hpc_system_status->get_current_num_avaibable_nodes() / (1.0 * job_description->get_num_nodes()));
+    // FIXME: again this is equivalent to the previous score
+    // mem_compat  = min(1.0, machine.available_memory / job.memory)
+    // TODO see if we can add storage (not a the moment)
+    // storage_compat = min(1.0, machine.available_storage / job.storage)
+
+    // 4. Machine type compatibility
+    // FIXME this is part of the acceptance tests and leads to 0.0 (job cannot execute)
+    // if job.requires_gpu and not machine.has_gpu:
+    //     type_score = 0.2
+    // else:
+    //     type_score = 1.0
+
+    // 5. Queue length factor
+    auto queue_factor = std::max(0.1, 1 - 0.1 * hpc_system_status->get_queue_length());
+
+    // 6. Combine (basic multiplicative form)
+    auto base_score = node_score * node_compat;
+    // base_score = (
+    //     (node_score + mem_score + stor_score) / 3.0
+    // ) * node_compat * mem_compat * storage_compat * type_score
+    std::cout << "min(1.0, " << node_score << " * " << node_compat << " * " << queue_factor << ")" << std::endl;
+    auto final_bid = std::min(1.0, base_score * queue_factor);
+
+    return std::trunc(final_bid * 100.0) / 100.0;
   }
 
   void broadcast_bid_on_job(const std::shared_ptr<wrench::S4U_Daemon>& bidder,
