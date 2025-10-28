4bc5b9734125ea20a1aa788e79d62f6562460956
Fred Suter <suterf@ornl.gov>
2025-10-28 13:17:50 -0400

    NEED TO BE REVISED AGAIN

--- python_scripts/HeuristicBidding.py
+++ python_scripts/HeuristicBidding.py
@@ -6,6 +6,22 @@ def compute_bid(job, machine, current_time=0):
         machine (object): Machine characteristics with attributes 'available_nodes', 'used_nodes', 'node_limit', 'has_gpu', 'queue_length'.
         current_time (int): Current time in the scheduling system for wait time calculations.
     """
+    # FIXME Please use the names of the fields in JobDecription: "job_id", "user_id","group_id", "job_type",
+    #       "submission_time", "walltime", "num_nodes", "needs_gpu", "requested_memory_gb", "requested_storage_gb",
+    #       "hpc_site", "hpc_system"
+    
+    # FIXME Please use the names of the fields in HPC System description: "name", "type", "num_nodes",
+    #       "memory_amount_in_gb", "storage_amount_in_gb", "has_gpu", "network_interconnect"
+    
+    # FIXME Please use the names of the fields in HPC System status: "current_num_available_nodes",
+    #       "current_job_start_time_estimate", "queue_length"
+
+    # FIXME we don't send the current time to python, I can fix this
+
+    # FIXME we have an current estimation of the start time of the job, might be interesting to use it. 
+
+    # FIXME The objective is to have this heuristic in python as a fallback of the LLM-bidding, it has to work with the
+    # rest of the code, and it can't as it completely disregard what has been done/ 
     
     # Safely read job fields (job is a dict)
     nodes_req = job.get("Nodes", 0)
@@ -18,6 +34,7 @@ def compute_bid(job, machine, current_time=0):
 
     # 1. Feasibility check
     # if job needs more nodes than available AND requests GPU but machine has no GPU => infeasible
+    # FIXME also check that memory requirement is lower than machine available memory
     if nodes_req > machine.available_nodes and requested_gpu and not machine.has_gpu:
         return 0.0
 
@@ -26,6 +43,7 @@ def compute_bid(job, machine, current_time=0):
     node_score = 1 - node_util
 
     # 3. Compatibility (avoid division by zero)
+    # FIXME jobs always have a number of nodes, can't have division by 0
     node_compat = min(1.0, machine.available_nodes / nodes_req) if nodes_req else 1.0
 
     # 4. Queue length factor
@@ -34,6 +52,7 @@ def compute_bid(job, machine, current_time=0):
     # 5. Time-based priority factor (longer wait = higher priority)
     wait_time = current_time - submission_time
     # Scale wait time: 0-100 time units -> 1.0-2.0 multiplier
+    # FIXME what is the rationale of that formula? 
     time_factor = min(2.0, 1.0 + (wait_time / 100.0))
 
     # 6. Job-Resource compatibility factor
@@ -53,8 +72,11 @@ def compute_bid(job, machine, current_time=0):
 
     # 7. Site/System preference factor
     machine_site = getattr(machine, 'site', job_site)  # Assume machine knows its site
+    # FIXME that is not the case for the one above, I can fix it. 
+
     machine_name = getattr(machine, 'name', job_system)  # Assume machine knows its name
     
+    # FIXME what the rationale of giving higher preference to local scheduling?
     if job_site == machine_site and job_system == machine_name:
         site_factor = 1.0  # Perfect match: same site and system
     elif job_site == machine_site:
