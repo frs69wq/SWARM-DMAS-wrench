4f8460422cea575a61c1982d6cbc39d0903af968
Fred Suter <suterf@ornl.gov>
2025-11-10 16:59:19 -0500

    add node speed to HPC system description

--- include/info/HPCSystemDescription.h
+++ include/info/HPCSystemDescription.h
@@ -11,6 +11,7 @@ class HPCSystemDescription {
   std::string site_;
   HPCSystemType type_;
   size_t num_nodes_;
+  double node_speed_;
   int memory_amount_in_gb_;
   int storage_amount_in_gb_;
   bool has_gpu_;
@@ -18,12 +19,13 @@ class HPCSystemDescription {
 
 public:
   HPCSystemDescription(const std::string& name, const std::string& site, HPCSystemType type, size_t num_nodes,
-                       int memory_amount_in_gb, int storage_amount_in_gb, bool has_gpu,
+                       double node_speed, int memory_amount_in_gb, int storage_amount_in_gb, bool has_gpu,
                        const std::string& network_interconnect)
       : name_(name)
       , site_(site)
       , type_(type)
       , num_nodes_(num_nodes)
+      , node_speed_(node_speed)
       , memory_amount_in_gb_(memory_amount_in_gb)
       , storage_amount_in_gb_(storage_amount_in_gb)
       , has_gpu_(has_gpu)
@@ -38,6 +40,7 @@ public:
     auto system_type = HPCSystemDescription::string_to_hpc_system_type(
         wrench::S4U_Simulation::getClusterProperty(system_name, "type"));
     auto system_num_compute_nodes = host_list.size() - 1;
+    auto system_node_speed = wrench::S4U_Simulation::getHostFlopRate(host_list.front());
     auto system_memory_amount_in_gb =
         std::stoi(wrench::S4U_Simulation::getClusterProperty(system_name, "memory_amount_in_gb"));
     auto system_storage_amount_in_gb =
@@ -47,8 +50,9 @@ public:
         wrench::S4U_Simulation::getClusterProperty(system_name, "network_interconnect");
 
     return std::make_shared<HPCSystemDescription>(system_name, system_site, system_type, system_num_compute_nodes,
-                                                  system_memory_amount_in_gb, system_storage_amount_in_gb,
-                                                  system_has_gpu, system_network_interconnect);
+                                                  system_node_speed, system_memory_amount_in_gb,
+                                                  system_storage_amount_in_gb, system_has_gpu,
+                                                  system_network_interconnect);
   }
 
   // Getters
@@ -57,6 +61,7 @@ public:
   const std::string& get_site() const { return site_; }
   HPCSystemType get_type() const { return type_; }
   size_t get_num_nodes() const { return num_nodes_; }
+  double get_node_speed() const { return node_speed_; }
   int get_memory_amount_in_gb() const { return memory_amount_in_gb_; }
   int get_storage_amount_in_gb() const { return storage_amount_in_gb_; }
   const std::string& get_network_interconnect() const { return network_interconnect_; }
@@ -98,6 +103,7 @@ public:
             {"site", site_},
             {"type", hpc_system_type_to_string(type_)},
             {"num_nodes", num_nodes_},
+            {"node_speed", node_speed_},
             {"memory_amount_in_gb", memory_amount_in_gb_},
             {"storage_amount_in_gb", storage_amount_in_gb_},
             {"has_gpu", has_gpu_},
